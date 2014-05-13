c0b17bbe2902883cf1f05bd380b9ea7704076287
William Robson <logiblocs@gmail.com>
2014-05-13 13:48:21 +0100

    First commit

---
+++ src/main/java/net/minecartrapidtransit/stocks/Stocks.java
@@ -0,0 +1,16 @@
+package net.minecartrapidtransit.stocks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Stocks extends JavaPlugin {
+	
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+}
